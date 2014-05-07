834cfc56cfd433662ef0d23318cf0de2164fd473
Alexander Belov <git@belov.by>
2014-05-08 01:02:36 +0300

    add test ruby files

---
+++ test1.rb
@@ -0,0 +1,34 @@
+a = 1+2
+b
+c = 2+3
+d
+
+def func1(a,b)
+	a = b + 1
+end
+
+def func2 a,b
+	a = b + 1
+	return b
+end
+
+def func3
+	return 1
+end
+
+func4(a)
+func5()
+
+def func3
+	return 2
+end
+
+e += 1
+
+n = []
+n[2] = 2
+k[2] = 1
+g = [1,3,4,5]
+
+a = 
+b = func1(1,
