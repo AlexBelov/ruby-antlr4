1ed785a57a4c4cd45e50e74fae9278cabab8abf9
Alexander Belov <git@belov.by>
2014-05-07 20:05:58 +0300

    add ruby preprocessing script (for require) and correct shell script run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
-antlr4 ruby.g4
-javac ruby*.java
-grun ruby prog -gui test.rb
+#!/bin/bash
+source ~/.rvm/scripts/rvm 1>/dev/null
+rvm use 2.0.0@pushkin-contest 1>/dev/null
+ruby preproc.rb
+java -jar /usr/local/lib/antlr-4.2.2-complete.jar Ruby.g4
+javac Ruby*.java
+java -classpath /usr/local/lib/antlr-4.2.2-complete.jar:. org.antlr.v4.runtime.misc.TestRig Ruby prog -gui $1
