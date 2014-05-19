86cea6828ca02b59a4fe9bcb19027843efacbb77
Alexander Belov <git@belov.by>
2014-05-19 22:25:05 +0300

    add script for writing antlr4 tree to file

--- test3.rb
+++ test3.rb
@@ -1,7 +1,1 @@
-def func
-	b = 1
-end
-
 a = 100+2*3
-
-func()
---
+++ tree.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source ~/.rvm/scripts/rvm 1>/dev/null
+rvm use 2.0.0@pushkin-contest 1>/dev/null
+ruby preproc.rb
+java -jar /usr/local/lib/antlr-4.2.2-complete.jar Ruby.g4
+javac Ruby*.java
+java -classpath /usr/local/lib/antlr-4.2.2-complete.jar:. org.antlr.v4.runtime.misc.TestRig Ruby prog -tree $1 1>tree.txt
+cat tree.txt
